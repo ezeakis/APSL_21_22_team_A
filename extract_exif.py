a02c786e5004d558c7bac15378e5cc2734edad03
ezeakis <ezeakis@gmail.com>
2022-02-16 14:50:17 +0200

    Update extract_exif.py

--- extract_exif.py
+++ extract_exif.py
@@ -6,27 +6,9 @@ print(exif_data)
 
 
 
-from PIL import Image, ExifTags
+import exifread
+# Open image file for reading (binary mode)
+f = open('gps0.jpg', 'rb')
 
-img = Image.open("gps0.jpg")
-img_exif = img.getexif()
-print(type(img_exif))
-# <class 'PIL.Image.Exif'>
-
-if img_exif is None:
-    print('Sorry, image has no exif data.')
-else:
-    for key, val in img_exif.items():
-        if key in ExifTags.TAGS:
-            print(f'{ExifTags.TAGS[key]}:{val}')
-            # ExifVersion:b'0230'
-            # ...
-            # FocalLength:(2300, 100)
-            # ColorSpace:1
-            # ...
-            # Model:'X-T2'
-            # Make:'FUJIFILM'
-            # LensSpecification:(18.0, 55.0, 2.8, 4.0)
-            # ...
-            # DateTime:'2019:12:01 21:30:07'
-            # ...
+# Return Exif tags
+tags = exifread.process_file(f)
